0173bf549bf918ed3368a479b8f6a7a4bf250bfd
baboucarr <baboucarrd43@gmail.com>
2018-07-14 23:34:14 +0000

    landing page for fcc

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+    function opennav() {
+        var element = document.querySelector(".nav");
+        if (element.style.display == "none") {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    }
+
